36d14ac80685f9a7f9c20d1a480241fa6c781b6c
vidyesh <vidyesh95@gmail.com>
2021-10-11 22:33:35 +0530

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    val char = 'c'
+    val ascii= char.code
+
+    // Kotlin program to find ASCII value of a character
+    println("The ASCII value of $char is: $ascii")
+}
